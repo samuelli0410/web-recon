7438a325373e8fe5fa9e120c7147b40e868d162e
Arjun Banerjee <abaner@berkeley.edu>
2023-04-11 00:05:10 -0700

    spiral spider movement

--- SpiderMovements.py
+++ SpiderMovements.py
@@ -11,22 +11,10 @@ spiderMovements = PlaneStructure.SpaceAndTime()
 
 spiderMovements.nextSecond(0,0)
 
-
-#Draw the first pattern:
-
-for x in range(10):
-    spiderMovements.nextSecond(x, np.sin(x*(np.pi/2)))
-
-
-#Draw the second pattern
-for x in range(5):
-    spiderMovements.nextSecond(x + 10, 10*np.sin(10*np.pi) +x**2)
-
-#Draw the last pattern
-
-for x in range (11):
-    spiderMovements.nextSecond(15-x, np.sin(10*np.pi) +5**2 + x)
-
+t = 0
+while t <= 35:
+    spiderMovements.nextSecond(t*np.cos(t), t*np.sin(t))
+    t += 0.25
 
 
 spiderMovements.draw2D()
