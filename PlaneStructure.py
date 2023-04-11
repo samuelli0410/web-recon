cc42556b41474f68fabb003fec5c8a4216c4782b
Arjun Banerjee <abaner@berkeley.edu>
2023-04-10 17:33:06 -0700

    added a to google sheets feature

--- PlaneStructure.py
+++ PlaneStructure.py
@@ -1,7 +1,8 @@
 # This file data structure that tracks the spiders movement
 import matplotlib.pyplot as plt
+import pandas as pd
 
-
+#TODO: Think about eliminating Position and Instead Making Double Lists
 
 # Location Class to Find Position
 class Position:
@@ -42,15 +43,35 @@ class SpaceAndTime:
     def entries(self):
         return len(self.spiderMovement)
 
-    def draw2D(self):
+
+    def getLists(self):
         x = [self.get(i).getX() for i in range(self.entries())]
         y = [self.get(i).getY() for i in range(self.entries())]
+        t = [i for i in range(self.entries())]
+        return t, x, y
+
+    def draw2D(self):
+        t, x, y = self.getLists()
         plotted = plt.plot(x,y)
         plt.show()
 
 
 
-    def toSheets(self):
-        return
+    def lists_to_excel(self, file_name):
+        #MadeByChatGPT
+
+        t, x, y = self.getLists()
+
+        # create a dataframe from the three lists
+        df = pd.DataFrame({'Time': t, 'X': x, 'Y': y})
+
+        # create a writer object for Excel
+        writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+
+        # write the dataframe to Excel
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        # save the Excel file
+        writer.save()
 
 
