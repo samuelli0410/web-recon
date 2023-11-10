4e5dae13cd04764e8e2749cd7508df6890b1cda0
Grant Yang <gyang276@berkeley.edu>
2023-11-10 13:56:38 -0800

    file system monitoring

---
+++ file_transfer/file_upload_handler.py
@@ -0,0 +1,51 @@
+"""Courtesy of ChatGPT"""
+
+import boto3
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+
+# Initialize S3 client
+s3_client = boto3.client('s3')
+
+# Replace with your S3 bucket name
+bucket_name = 'spider-video'
+
+class UploadEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Detected new file: {event.src_path}")
+        if not event.is_directory:
+            self.upload_file(event.src_path)
+
+    def upload_file(self, file_path):
+        file_name = os.path.basename(file_path)
+        print(f'Uploading {file_name} to S3 bucket {bucket_name}...')
+        start_time = time.time()
+        try:
+            s3_client.upload_file(file_path, bucket_name, file_name)
+            print(f'{file_name} uploaded.')
+            end_time = time.time()
+            print(f"Upload took {end_time - start_time} seconds to complete.")
+            print()
+            
+        except Exception as e:
+            print(f"Error during upload: {e}")
+
+# Set up the observer
+path = os.path.expanduser('~/Documents/spider-recordings')
+event_handler = UploadEventHandler()
+observer = Observer()
+observer.schedule(event_handler, path, recursive=False)
+
+# Start the observer
+observer.start()
+print('Monitoring folder for new video files...')
+try:
+    while True:
+        # Run indefinitely
+        pass
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
