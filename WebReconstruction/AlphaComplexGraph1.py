3e81afa6f272e9474469cb4c7323b5c1a5c787ca
Samuel Li <chengda.li@berkeley.edu>
2025-05-10 18:46:23 -0700

    documentation added

--- WebReconstruction/AlphaComplexGraph1.py
+++ WebReconstruction/AlphaComplexGraph1.py
@@ -3,3 +3,4 @@
 #Build alpha complexes of varying radius epsilon
 #Use preserved triangulations to determine where likely holes are
 #Prims/spanning tryy and then poke the holes in afterwards
+#https://gudhi.inria.fr/python/latest/delaunay_complex_ref.html#gudhi.AlphaComplex
