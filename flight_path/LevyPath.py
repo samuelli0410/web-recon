63ad24874e9e16887962caa649c1ab00be69b0fe
Samuel Li <chengda.li@berkeley.edu>
2025-07-26 20:37:06 -0700

    7/26 faster levy walk

--- flight_path/LevyPath.py
+++ flight_path/LevyPath.py
@@ -31,4 +31,23 @@ def simulate_levy_walk(starting_pos=[0,0,0], timesteps=10,alpha = 2, step_scale=
     return res
 
 
-print(simulate_levy_walk())
+def simulate_levy_walk_fast(starting_pos=[0,0,0], timesteps=10, alpha=2, step_scale=1):
+    starting_pos = np.array(starting_pos)
+    positions = np.zeros((timesteps+1, 3))
+    positions[0] = starting_pos
+    
+    # Generate all random directions (Gaussian then normalize)
+    directions = np.random.normal(size=(timesteps, 3))
+    norms = np.linalg.norm(directions, axis=1).reshape(-1, 1)
+    directions /= norms
+    
+    # Generate all step lengths with levy steps
+    u = np.random.uniform(size=timesteps)
+    step_lengths = step_scale * (u ** (-1/alpha))
+    steps = directions * step_lengths.reshape(-1, 1)
+    positions[1:] = starting_pos + np.cumsum(steps, axis=0)
+    
+    return positions
+
+
+print(simulate_levy_walk_fast())
